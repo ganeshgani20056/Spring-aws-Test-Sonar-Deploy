ddfc74f2e33eb79983bdf5623ec75bb247f9d90b
Ganesh Kayala <23MH1A0525@acoe.edu.in>
2026-02-28 21:40:57 +0530

    code change

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CseController {
-	@GetMapping("/cseadd")
-	public int addCSE(@RequestParam int a, @RequestParam int b) {
-		return a + b;
-	}
+
+    @GetMapping("/cseadd")
+    public Map<String, Object> addCSE(@RequestParam int a, @RequestParam int b) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("result", a + b);
+        response.put("designed_by", "23MH1A0525");
+
+        return response;
+    }
 }
--- src/test/java/com/example/demo/CseControllerTest.java
+++ src/test/java/com/example/demo/CseControllerTest.java
@@ -1,18 +1,24 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CseControllerTest {
-	@Autowired
-	CseController c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
 
+    @Autowired
+    CseController c;
+
+    @Test
+    void test() {
+        Map<String, Object> response = c.addCSE(2, 3);
+
+        assertEquals(5, response.get("result"));
+        assertEquals("23MH1A0525", response.get("designed_by"));
+    }
 }
